7b2b46c631bd0a3a7acbc5055a055325d48f9379
Areydra Desfikriandre <areydra@gmail.com>
2020-07-11 20:59:13 +0700

    change concept firebaseConfig

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import Router from './routes/index'
 import firebase from 'firebase'
-
-import { apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId } from './key'
-
-// Your web app's Firebase configuration
-const firebaseConfig = { apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId }
+import firebaseConfig from './key';
 
 // Initialize Firebase
 if (!firebase.apps.length) { //check if not initialize
