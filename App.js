26ee66580f2a8a93925473e64299356bfc0b8eac
Areydra Desfikriandre <areydra@gmail.com>
2020-07-12 10:58:45 +0700

    move all key firebase config to .env

--- App.js
+++ App.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import Router from './routes/index'
-import firebase from 'firebase'
-import firebaseConfig from './key';
+import firebase from 'firebase';
+import ENV from 'react-native-config';
+
+import Router from './routes/index';
+
+const firebaseConfig = {
+  apiKey: ENV.FIREBASE_API_KEY,
+  authDomain: ENV.FIREBASE_AUTH_DOMAIN,
+  databaseURL: ENV.FIREBASE_DATABASE_URL,
+  projectId: ENV.FIREBASE_PROJECT_ID,
+  storageBucket: ENV.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: ENV.FIREBASE_MESSAGING_SENDER_ID,
+  appId: ENV.FIREBASE_APP_ID,
+  measurementId: ENV.FIREBASE_MEASUREMENT_ID
+}
 
 // Initialize Firebase
 if (!firebase.apps.length) { //check if not initialize
