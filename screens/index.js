02db0fb64cd919781f72c2b13fe26b104410b9e0
Areydra Desfikriandre <areydra@gmail.com>
2021-02-07 13:11:02 +0700

    delete screen register

--- screens/index.js
+++ screens/index.js
@@ -3,9 +3,8 @@ import Friends from './Friends'
 import Map from './Map'
 import Messages from './Messages'
 import Profile from './Profile'
-import Register from './Register'
 import Splash from './Splash'
 
 export{
-    Chat, Friends, Map, Messages, Profile, Register, Splash
+    Chat, Friends, Map, Messages, Profile, Splash
 }
