4e7bafdb6daae8e684f9421c2b7dd5112c5a9d04
Areydra Desfikriandre <areydra@gmail.com>
2021-02-09 21:00:32 +0700

    fix title style

--- src/screens/MapScreen/styles.js
+++ src/screens/MapScreen/styles.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
     },
     title: {
         flex: 1,
-        marginLeft: -20,
+        marginRight: 20,
         textAlign: 'center',
     },
     locationContainer: {
