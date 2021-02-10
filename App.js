5f9e3b727f1b42d0cb5ec60b80c2c173b72b36bb
Areydra Desfikriandre <areydra@gmail.com>
2021-02-10 20:15:27 +0700

    fix wrong attribute for Provider

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {Provider} from 'react-redux';
 import {store} from './redux';
 
 const App = () => (
-    <Provider value={store}>
+    <Provider store={store}>
         <Router/>
     </Provider>
 );
